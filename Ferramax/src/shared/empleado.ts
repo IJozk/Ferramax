9a11b76a8f9ebb7f22b94bd23117ab81bce9bbb5
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2024-05-02 22:45:35 -0400

    empleado.ts creado

---
+++ Ferramax/src/shared/empleado.ts
@@ -0,0 +1,13 @@
+import { Entity, Fields } from "remult";
+
+@Entity("empleados", { allowApiCrud: true })
+export class Empleados {
+    @Fields.autoIncrement()
+    id = 0;
+    @Fields.string()
+    name = "";
+    @Fields.date()
+    fechanacimiento = new Date();
+    @Fields.createdAt()
+    createdAt?: Date
+}
