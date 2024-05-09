b224c72570800724c2559f8f8a9be7da320881a6
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2024-05-08 23:42:02 -0400

    Database step finished
    It was just requiered to download "pg" as a dependency

--- Ferramax/src/server/api.ts
+++ Ferramax/src/server/api.ts
@@ -5,10 +5,14 @@ import { Client } from '../shared/Client'
 import { Employee } from '../shared/Employee'
 import { Product } from '../shared/Product'
 import { ProductsController } from '../shared/ProductsController'
+import { createPostgresDataProvider } from 'remult/postgres'
 
 export const api = remultExpress({
     entities: [Client, Employee, Product],
     admin: true,
     controllers: [ProductsController],
-    getUser: req => req.session!["user"]
+    getUser: req => req.session!["user"],
+    dataProvider: createPostgresDataProvider({
+        connectionString: ""
+    })
 })
