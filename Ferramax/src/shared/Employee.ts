6caa73421445846121462240d0e012dfe472d858
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2024-05-11 22:15:49 -0400

    add restricions to eployee

--- Ferramax/src/shared/Employee.ts
+++ Ferramax/src/shared/Employee.ts
@@ -9,6 +9,22 @@ export class Employee {
         validate: Validators.required
     })
     name = "";
+
+    @Fields.string({
+        validate: (employee) => {
+            if (employee.prod_name.length < 3) throw "Nombre demasiado corto"
+        },
+        allowApiUpdate: "admin",
+    })
+    last_name = "";
+
+    @Fields.string({
+        validate: (employee) => {
+            if (employee.prod_name.length < 3) throw "Nombre demasiado corto"
+        },
+        allowApiUpdate: "admin",
+    })
+    mail = "";
     
     @Fields.date()
     birthday = new Date();
