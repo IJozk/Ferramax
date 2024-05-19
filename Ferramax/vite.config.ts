a98de7752e8a39b0fcb354f23cdc950f0208b912
JorgeRspk <jriffo@spkdata.cl>
2024-05-19 19:18:10 -0400

    Feat - First Api Get

--- Ferramax/vite.config.ts
+++ Ferramax/vite.config.ts
@@ -13,5 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: { proxy: { "/api": "http://localhost:3002" } },
+  server: { proxy: { "/api": {
+    target: 'https://a664615f-f965-4661-ae43-67e040721d30-00-s66hv8tg1s0d.kirk.replit.dev:3001',
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, '')
+  } } },
 })
